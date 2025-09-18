410386ad129a15080b5372b81f17834a5519c3c1
Said Simokovic <said.simokovic@digitecgalaxus.ch>
2025-09-18 16:08:16 +0200

    new version with bold text

--- src/CoolComponent.js
+++ src/CoolComponent.js
@@ -1,3 +1,3 @@
 export default coolComponent = ({text = "default text. you can give the 'text' parameter a value"}) => {
-    return <div style={{color: "blue"}}>{text}</div>
+    return <div style={{color: "blue", fontWeight: "bold"}}>{text}</div>
 }
